7916997348ec5d5da18ac556f1a1f7ae67a19b34
R S <rena_schott@gmx.de>
2023-12-18 17:27:26 +0000

    Made to login to the admin panel possible

--- bg_review/admin.py
+++ bg_review/admin.py
@@ -18,21 +18,21 @@ class BoardGameAdmin(admin.ModelAdmin):
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'bg_name', 'created_on', 'content')
-    list_filter = ('author', 'due_back')
+    list_filter = ('author', 'created_on')
     search_fields = ['author', 'bg_name']
 
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
     list_display = ('user', 'rating', 'review', 'help_text')
-    list_filter = ('user', 'due_back')
+    list_filter = ('user', 'review')
     search_fields = ['user', 'rating']
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('content', 'created_on', 'guest', 'blog')
-    list_filter = ('blog', 'due_back',)
+    list_filter = ('blog', 'created_on')
     search_fields = ['blog']
 
 
@@ -42,8 +42,8 @@ class UserAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(BoardGame, BoardGameAdmin)
-admin.site.register(Review, ReviewAdmin)
-admin.site.register(Rating, RatingAdmin)
-admin.site.register(Comment, CommentAdmin)
-admin.site.register(User, UserAdmin)
+#admin.site.register(BoardGame, BoardGameAdmin)
+#admin.site.register(Review, ReviewAdmin)
+#admin.site.register(Rating, RatingAdmin)
+#admin.site.register(Comment, CommentAdmin)
+#admin.site.register(User, UserAdmin)
