c21f96f79dc34205926c7f945db4ac7ff27d531a
R S <rena_schott@gmx.de>
2023-12-19 14:00:14 +0000

    Prepared for synamic page linking with id

--- bg_review/urls.py
+++ bg_review/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:id>', views.home, name='home'),
 ]
--- bg_review/views.py
+++ bg_review/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BoardGame, Review, Rating, Comment, User
 
 
 
 # Create your views here.
+#homepage
 def home(request):
     return render(request, 'index.html')
+
+
+#specific board game review
+def boardgame(request, id):
+    ls = Review.objects.get(id=id)
+    return render(request, '' % ls.name)
